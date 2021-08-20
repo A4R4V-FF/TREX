2ccb1b57ae748a433ac5257d12f837c950b4f5f0
A4R4V-FF <89017796+A4R4V-FF@users.noreply.github.com>
2021-08-20 14:58:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,19 @@
-var btn_red;
-var btn_green;
 
-var r = 0;
-var g = 0;
-var b = 0;
-
-function setup() {
-  createCanvas(400, 400);
-  btn_red = createButton("RED");
-  btn_red.position(100,50);
-  btn_red.mousePressed(red_bg);
- 
-  btn_green = createButton("GREEN");
-  btn_green.position(200,50);
-  btn_green.mousePressed(green_bg);
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 }
 
+function setup(){
+  createCanvas(400,400);
+  //spriteName = addImage(seaImg);
+
+}
+spriteName = addImage(seaImg);
+if (seaImg.x<0) {
+  seaImg.x = sea.width/2;
+}
 function draw() {
-  background(r,g,b);
-
-  function red_bg()
-{
- r = 255;
- g = 0;
- b = 0;
+  background("blue");
+ 
 }
-
-function green_bg()
-{
- r = 0;
- g = 255;
- b = 0;
-}
-drawSprites();
-}
-
-
