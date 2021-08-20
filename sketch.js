c41413161670c7c1dc4a9a3c81d16c1abcca00be
A4R4V-FF <89017796+A4R4V-FF@users.noreply.github.com>
2021-08-20 15:52:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
-
+var sea;
+var ship;
+var seaImg;
+var shipImg1;
 function preload(){
 seaImg = loadImage("sea.png");
 shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
@@ -7,13 +10,24 @@ shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
 function setup(){
   createCanvas(400,400);
   //spriteName = addImage(seaImg);
-
-}
-spriteName = addImage(seaImg);
-if (seaImg.x<0) {
-  seaImg.x = sea.width/2;
-}
-function draw() {
+  sea = createSprite(400,200);
   background("blue");
- 
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale = 0.4;
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation( "movingShip" , shipImg1);
+  ship.scale = 0.3;
+}
+
+
+
+function draw() {
+ backgroung(0);
+ sea.velocityX = -3;
+  if (seaImg.x<0) {
+    seaImg.x = sea.width/2;
+  
+}
+
 }
